342e6c476e0786b02798e8e350549b73178c3679
julian <juliyachedev@gmail.com>
2022-09-30 14:58:47 -0300

    pagination /post GET

--- backend/src/Interfaces/repository.interface.ts
+++ backend/src/Interfaces/repository.interface.ts
@@ -2,11 +2,17 @@ import { BaseEntity, UpdateResult } from 'typeorm'
 
 export type Query = Record<string, any>
 
+export interface QueryList {
+  size: number
+  page: number
+  sort: any
+}
+
 export type Id = number
 
 export interface DatabaseRepository<Entity extends BaseEntity> {
   create(data: Entity, query?: Query): Promise<Entity>
-  list(query?: Query): Promise<Entity[]>
+  list(query?: Query | QueryList): Promise<Entity[]>
   get(id: Id, query?: Query): Promise<Entity | null>
   update(id: Id, data: Partial<Entity>, query?: Query): Promise<UpdateResult>
   remove(id: Id, query?: Query): Promise<UpdateResult>
