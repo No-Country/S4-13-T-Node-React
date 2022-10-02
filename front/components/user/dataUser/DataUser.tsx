a83f4fd47ec10b68b75cc2a4339871cc1cbecc13
Jose Sandino <josesandinonio@gmail.com>
2022-10-02 01:01:26 -0600

    profile view

---
+++ front/components/user/dataUser/DataUser.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function DataUser() {
+  return (
+    <div className='flex flex-col mt-11'>
+        <div className='border-b border-accent w-[173px] pb-6'>
+            <h2 className='text-text font-normal text-xl font-orelega'>Chino48</h2>
+            <h5 className='font-roboto text-sm'>chinotoresi@gmail.com</h5>
+        </div>
+        <a className='font-roboto font-bold text-primary text-base mt-6' href="">Editar perfil</a>
+    </div>
+  )
+}
+
+export default DataUser
