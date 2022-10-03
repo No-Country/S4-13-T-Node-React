2c1a759d4b7beb7aecd608de88322843d8764a71
julian <juliyachedev@gmail.com>
2022-10-03 20:57:47 -0300

    fix

--- backend/src/Config/config.ts
+++ backend/src/Config/config.ts
@@ -2,19 +2,6 @@ import * as dotenv from 'dotenv'
 import path from 'path'
 import { DataSource } from 'typeorm'
 
-dotenv.config()
-
-export const ENV = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: Number(process.env.PORT) || 8080,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_PORT: Number(process.env.DB_PORT) || 5432,
-  DB_DATABASE: process.env.DB_DATABASE,
-  ARG1: process.argv[2],
-}
-
 export abstract class ConfigServer {
   public NODE_ENV
 
