304a7cf2d3842d26548c7b166c90c4170129f819
Rodrigo Spano <rorrospano17@gmail.com>
2022-10-04 15:01:09 -0300

    .

--- front/components/navbar/menu/hamburguerMenu/ItemMenu.tsx
+++ front/components/navbar/menu/hamburguerMenu/ItemMenu.tsx
@@ -6,7 +6,7 @@ export const ItemMenu = (props: MenuItem) => {
   return (
     <div className="py-3">
       <Link href={props.href}>
-        <a className="cursor-pointer text-white capitalize font-orelega text-[20px] font-normal">{props.text}</a>
+        <a className="cursor-pointer text-white capitalize font-orelega text-[20px] font-normal ">{props.text}</a>
       </Link>
     </div>
   );
