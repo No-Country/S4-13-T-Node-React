8fe00247cef84517d8d26819a1560c89469db196
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-10-21 17:52:56 -0300

    Adding Logout & Upload Succesful components

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Layout from '../components/layout/Layout';
 import PostsContainer from '../components/posts/PostsContainer';
-import EditProfile from '../components/user/EditProfile';
 import { useAxios } from '../hooks/useAxios';
 import { AxiosGetPost, IPost } from '../interfaces';
 import { handleModal } from '../redux/slice/modalSlice';
